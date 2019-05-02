394a12b71a2a3f6cf078a20b7092ed5d27720d91
Karan <karan@bbtdigital.com>
2019-05-02 12:49:56 +1200

    [*] add extensions for filters

--- src/CMS/Search/SearchForm.php
+++ src/CMS/Search/SearchForm.php
@@ -11,7 +11,9 @@ namespace SilverStripers\ElementalSearch\CMS\Search;
 
 use BadMethodCallException;
 use SilverStripe\Assets\File;
+use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\CMS\Search\SearchForm as SS_SearchForm;
+use SilverStripe\ORM\DB;
 use SilverStripers\ElementalSearch\Model\SearchDocument;
 
 class SearchForm extends SS_SearchForm
@@ -24,7 +26,7 @@ class SearchForm extends SS_SearchForm
 
 	public function classesToSearch($classes)
 	{
-		$supportedClasses = array(SearchDocument::class, File::class);
+		$supportedClasses = array(SearchDocument::class);
 
 		$illegalClasses = array_diff($classes, $supportedClasses);
 		if ($illegalClasses) {
@@ -36,5 +38,91 @@ class SearchForm extends SS_SearchForm
 		$legalClasses = array_intersect($classes, $supportedClasses);
 		$this->classesToSearch = $legalClasses;
 	}
+	
+	/**
+	 * Return dataObjectSet of the results using current request to get info from form.
+	 * Wraps around {@link searchEngine()}.
+	 *
+	 * @return SS_List
+	 */
+	public function getResults()
+	{
+		// Get request data from request handler
+		$request = $this->getRequestHandler()->getRequest();
+		
+		// set language (if present)
+		$locale = null;
+		$origLocale = null;
+		if (class_exists('Translatable')) {
+			$locale = $request->requestVar('searchlocale');
+			if (SiteTree::singleton()->hasExtension('Translatable') && $locale) {
+				if ($locale === "ALL") {
+					Translatable::disable_locale_filter();
+				} else {
+					$origLocale = Translatable::get_current_locale();
+					
+					Translatable::set_current_locale($locale);
+				}
+			}
+		}
+		
+		$keywords = $request->requestVar('Search');
+		
+		$andProcessor = function ($matches) {
+			return ' +' . $matches[2] . ' +' . $matches[4] . ' ';
+		};
+		$notProcessor = function ($matches) {
+			return ' -' . $matches[3];
+		};
+		
+		$keywords = preg_replace_callback('/()("[^()"]+")( and )("[^"()]+")()/i', $andProcessor, $keywords);
+		$keywords = preg_replace_callback('/(^| )([^() ]+)( and )([^ ()]+)( |$)/i', $andProcessor, $keywords);
+		$keywords = preg_replace_callback('/(^| )(not )("[^"()]+")/i', $notProcessor, $keywords);
+		$keywords = preg_replace_callback('/(^| )(not )([^() ]+)( |$)/i', $notProcessor, $keywords);
+		
+		$keywords = $this->addStarsToKeywords($keywords);
+		
+		$pageLength = $this->getPageLength();
+		$start = $request->requestVar('start') ?: 0;
+		
+		//add filters
+		$request_vars = $request->getVars();
+		$extraFilter = '';
+		$this->extend('updateSearchFormFilters', $request_vars, $extraFilter);
+		
+		$extra_file_filter = '';
+		$this->extend('updateExtraFileFilter', $request_vars, $extra_file_filter);
+		
+		$booleanSearch =
+			strpos($keywords, '"') !== false ||
+			strpos($keywords, '+') !== false ||
+			strpos($keywords, '-') !== false ||
+			strpos($keywords, '*') !== false;
+		$results = DB::get_conn()->searchEngine($this->classesToSearch, $keywords, $start, $pageLength, "\"Relevance\" DESC", $extraFilter, $booleanSearch, $extra_file_filter);
+		
+		// filter by permission
+		if ($results) {
+			foreach ($results as $result) {
+				if (!$result->canView()) {
+					$results->remove($result);
+				}
+			}
+		}
+		
+		// reset locale
+		if (class_exists('Translatable')) {
+			if (SiteTree::singleton()->hasExtension('Translatable') && $locale) {
+				if ($locale == "ALL") {
+					Translatable::enable_locale_filter();
+				} else {
+					Translatable::set_current_locale($origLocale);
+				}
+			}
+		}
+		
+		return $results;
+	}
+	
+	
 
 }
